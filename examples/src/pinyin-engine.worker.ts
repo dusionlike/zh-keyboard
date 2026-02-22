1d8de91e6d2240b4e5122f400a3257fd3289ab37
dusionlike <751069244@qq.com>
2026-02-22 20:56:05 +0800

    chore: 修复 github pages 的演示程序无法加载拼音引擎的问题

--- examples/src/pinyin-engine.worker.ts
+++ examples/src/pinyin-engine.worker.ts
@@ -2,7 +2,7 @@ import { RimePinyinEngine } from '@zh-keyboard/pinyin'
 import { expose } from 'comlink'
 
 const pinyinEngine = new RimePinyinEngine({
-  wasmDir: `${import.meta.env.BASE_URL}data`,
+  wasmDir: import.meta.env.DEV ? '/data' : './data',
 })
 
 expose(pinyinEngine)
